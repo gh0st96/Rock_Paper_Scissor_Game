52ed02095c97e03c3ab24bf0af2bf2ffbe268b85
Achintha Lakshan <127497340+gh0st96@users.noreply.github.com>
2024-08-24 10:42:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,6 +52,15 @@ function getRandomComputerResult() {
   
   };
   function resetGame() {
+      playerScore=0;
+  computerScore=0;
+  playerScoreSpanElement.innerText=playerScore;
+  computerScoreSpanElement.innerText=computerScore;
+  resetGameBtn.style.display = "none";
+  optionsContainer.style.display = "block";
+  winnerMsgElement.innerText="";
+  roundResultsMsg.innerText=""
+
   
   };
   
